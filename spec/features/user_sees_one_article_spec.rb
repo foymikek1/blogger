3552cafb46354f8009e13fa50d212cb66091d25e
foymikek <foymikek@gmail.com>
2025-04-04 10:20:18 -0600

    Add test spec to confirm user can see article comments.

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,7 +4,9 @@ describe "user sees one article", type: :feature do
   describe "they click a link from articles index" do
     it 'takes them to a new page showing one article' do
       article = Article.create!(title: "New Title", body: "New Body")
-
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
+      
       visit articles_path
       click_link article.title
 
